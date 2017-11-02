578998e7c5aed2467dc612084f74f4885d00e230
Binita Mehta <binita_mehta@yahoo.com>
2017-11-02 14:33:43 -0400

    Changes to post selected number to the server.

--- app/view/view.js
+++ app/view/view.js
@@ -9,21 +9,50 @@ angular.module('myApp.view', ['ngRoute'])
   });
 }])
 
-.controller('ViewCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('ViewCtrl', ['$scope', '$http', '$q', function($scope, $http, $q) {
   var vm=this;
+  vm.loading = true;
   vm.fibonacciNumbers = [];
   vm.selectedNumber;
-  $http.get('http://localhost:3000/api/fibonacci')
+  vm.selectedNumberArray = [];
+
+  vm.request1 = $http.get('http://localhost:3000/api/fibonacci')
       .then(function(result){
         vm.fibonacciNumbers = result.data;
       }, function(error) {
         console.error('Could not fetch fibonacci numbers: ' + JSON.stringify(error));
-      })
+      });
+
+  vm.request2 = $http.get('http://localhost:3000/api/total')
+      .then(function(result){
+        vm.currentTotal= parseInt(result.data);
+      }, function(error){
+
+      });
+
+  $q.all([vm.request1, vm.request2]).then(function(){
+    vm.loading= false;
+  });
+
 
   vm.trackSelected = function(){
-    console.log(typeof vm.selectedNumber);
-    if (vm.fibonacciNumbers.indexOf(parseInt(vm.selectedNumber)) > -1) {
+    var selectedNo = parseInt(vm.selectedNumber);
+    if (vm.fibonacciNumbers.indexOf(selectedNo) > -1) {
         console.log(vm.selectedNumber);
+        var data = '{"number":' +  selectedNo + '}';
+        console.log(data);
+
+        var config = {headers: {
+            'Content-type': 'application/text'
+        }};
+
+        $http.post('http://localhost:3000/api/post', data, config).then(function(){
+            console.log('posted data successfully');
+            vm.selectedNumberArray.push(selectedNo);
+            vm.currentTotal += selectedNo;
+        }, function(error){
+            console.error('Could not post data successfully: ' + JSON.stringify(error));
+        });
     }
 
   }
